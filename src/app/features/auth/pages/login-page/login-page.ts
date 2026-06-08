c8d537c15fecc56072b08f234c96e2a7844f5fc3
Vinicius Souza <viniciusouza7@gmail.com>
2026-06-07 22:26:01 -0300

    feat(auth): add login page with reactive forms and implement login flow integration with backend API

--- src/app/features/auth/pages/login-page/login-page.ts
+++ src/app/features/auth/pages/login-page/login-page.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthFacade } from '../../facades/auth.facade';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
-export class LoginPage {}
+export class LoginPage {
+  private readonly fb = inject(FormBuilder);
+  private readonly authFacade = inject(AuthFacade);
+
+  protected readonly loginForm = this.fb.group({
+    email: this.fb.nonNullable.control('', [Validators.required, Validators.email]),
+    password: this.fb.nonNullable.control('', [Validators.required, Validators.minLength(6)]),
+  });
+
+  protected onSubmit(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const request = this.loginForm.getRawValue();
+
+    this.authFacade.login(request).subscribe({
+      next: (response) => {
+        console.log('Login realizado com sucesso');
+        console.log(response);
+      },
+
+      error: (error) => {
+        console.error('Erro ao realizar login');
+        console.error(error);
+      },
+    });
+  }
+}
